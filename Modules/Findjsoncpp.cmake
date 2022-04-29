3df577282285fae4164d40938b4a22bb0799e02b
Chris Green <greenc@fnal.gov>
2022-04-29 12:59:10 -0500

    Minor specificity improvements

--- Modules/Findjsoncpp.cmake
+++ Modules/Findjsoncpp.cmake
@@ -1,21 +1,21 @@
 if (NOT ${CMAKE_FIND_PACKAGE_NAME}_FOUND)
-  set(_cet_find${CMAKE_FIND_PACKAGE_NAME}_required ${${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED})
+  set(_cet_fjcpp_required ${${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED})
   set(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
-  set(_cet_find${CMAKE_FIND_PACKAGE_NAME}_quietly ${${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY})
+  set(_cet_fjcpp_quietly ${${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY})
   find_package(${CMAKE_FIND_PACKAGE_NAME} CONFIG QUIET)
   if (${CMAKE_FIND_PACKAGE_NAME}_FOUND)
-    set(_cet_find${CMAKE_FIND_PACKAGE_NAME}_config_mode CONFIG_MODE)
+    set(_cet_fjcpp_config_mode CONFIG_MODE)
   else()
     if (DEFINED ENV{JSONCPP_FQ_DIR} AND EXISTS "$ENV{JSONCPP_FQ_DIR}/lib/pkgconfig/jsoncpp.pc")
       set(ENV{PKG_CONFIG_PATH} "$ENV{JSONCPP_FQ_DIR}/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}")
     endif()
     include(CetFindPkgConfigPackage)
-    cet_find_pkg_config_package(jsoncpp)
+    cet_find_pkg_config_package(jsoncpp QUIET)
   endif()
-  set(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED ${_cet_find${CMAKE_FIND_PACKAGE_NAME}_required})
-  set(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY ${_cet_find${CMAKE_FIND_PACKAGE_NAME}_required})
-  unset(_cet_find${CMAKE_FIND_PACKAGE_NAME}_required)
-  unset(_cet_find${CMAKE_FIND_PACKAGE_NAME}_quietly)
+  set(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED ${_cet_fjcpp_required})
+  set(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY ${_cet_fjcpp_quietly})
+  unset(_cet_fjcpp_required)
+  unset(_cet_fjcpp_quietly)
 endif()
 if (NOT ${CMAKE_FIND_PACKAGE_NAME}_FOUND)
   set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR $ENV{JSONCPP_INC})
@@ -31,13 +31,13 @@ endif()
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
-  ${_cet_find${CMAKE_FIND_PACKAGE_NAME}_config_mode}
+  ${_cet_fjcpp_config_mode}
   REQUIRED_VARS ${CMAKE_FIND_PACKAGE_NAME}_LIBRARIES
   ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIRS
   VERSION_VAR ${CMAKE_FIND_PACKAGE_NAME}_VERSION
   )
 
-unset(_cet_find${CMAKE_FIND_PACKAGE_NAME}_config_mode)
+unset(_cet_fjcpp_config_mode)
 
 if (${CMAKE_FIND_PACKAGE_NAME}_FOUND AND NOT TARGET jsoncpp_lib)
   if (TARGET PkgConfig::${CMAKE_FIND_PACKAGE_NAME})
